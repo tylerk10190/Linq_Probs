25f7f4040fc098e85b3471470d0557ed1dbedd44
Tyler Koepke <t.koepke10190@gmail.com>
2019-08-26 15:57:57 -0500

    completed problems 1-3

---
+++ LINQproblems/Problems.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQproblems
+{
+    public class Problems
+    {
+
+
+        public Problems()
+        {
+
+        }
+
+        public void FilterStrings()
+        {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var wordsWithTH = words.Where(w => w.Contains("th"));
+            foreach(var THwords in wordsWithTH)
+            {
+                Console.WriteLine(THwords);
+               
+            }
+            Console.ReadLine();
+        }
+
+        public List<string> ProblemTwo(List<string> names)
+        {
+
+            List<string> newNames = names.Distinct().ToList();
+            foreach (var item in newNames)
+            {
+                Console.WriteLine(item);    
+            }
+            Console.ReadLine();
+            return newNames;
+        }
+        
+        public void ProblemThree(List<string> classGrades)
+        {
+            List <double> gradeAvgs = new List<double>();
+            List<int> gradeHolder = new List<int>();
+            for(int i = 0; i < classGrades.Count; i++)
+            {
+                var Student = classGrades[i];
+                string [] StudentScores = Student.Split(',');
+                foreach(string score in StudentScores)
+                {
+                    gradeHolder.Add(int.Parse(score));
+                }
+                gradeHolder.Remove(gradeHolder.Min());
+                gradeAvgs.Add(gradeHolder.Average());
+                gradeHolder.RemoveRange(0,gradeHolder.Count);
+            }
+            Console.WriteLine(gradeAvgs.Average());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LINQproblems/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQproblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problems problem = new Problems();
+
+            //problem.FilterStrings();
+
+            //List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+
+            //problem.ProblemTwo(names);
+
+            List<string> classGrades = new List<string>() { "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" };
+
+            problem.ProblemThree(classGrades);
+        }
+    }
+}
